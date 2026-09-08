5eb9dca15cb54eaf5cd5ec1c3c1d481465def022
GregoryCMSV <121581844+GregoryCMSV@users.noreply.github.com>
2026-09-07 21:56:26 -0300

    Adicionado DTO CurrentGame
    Adicionado DTO CurrentGame

---
+++ Arthur_Showdown/Arthur_Showdown_Shared/Dtos/Game/CurrentGameResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arthur_Showdown_Shared.Dtos.Game
+{
+    public class CurrentGameResponse
+    {
+        public int? GameID { get; set; }
+    }
+}
