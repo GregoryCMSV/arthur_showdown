9afcb80314397c99270f08c64748477e5c637154
GregoryCMSV <121581844+GregoryCMSV@users.noreply.github.com>
2026-09-13 18:08:48 -0300

    Criado DTOs para personagem e os ataques vinculados
    Criado DTOs para personagem e os ataques vinculados

---
+++ Arthur_Showdown/Arthur_Showdown_Shared/Dtos/Game/AtaqueDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arthur_Showdown_Shared.Dtos.Game
+{
+    public class AtaqueDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+
+        public string Descricao { get; set; } = string.Empty;
+        public string? ImagemUrl { get; set; }
+        public string HitboxJson { get; set; } = string.Empty;
+        public int Dano { get; set; }
+        public int Cooldown { get; set; }
+        public bool IsFastAction { get; set; }
+        public int LimiteAprimoramento { get; set; }
+    }
+}
---
+++ Arthur_Showdown/Arthur_Showdown_Shared/Dtos/Game/PersonagemDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arthur_Showdown_Shared.Dtos.Game
+{
+    public class PersonagemDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string? ImagemUrl { get; set; }
+        public List<AtaqueDto> AtaquesIniciais { get; set; } = new();
+    }
+}
