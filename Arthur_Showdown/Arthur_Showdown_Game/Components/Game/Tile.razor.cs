79566f7a87f7c14e02bdd868d652e8055a7aa9b3
GregoryCMSV <121581844+GregoryCMSV@users.noreply.github.com>
2026-09-13 21:47:45 -0300

    Criado componente para tile
    Criado componente para tile

---
+++ Arthur_Showdown/Arthur_Showdown_Game/Components/Game/Tile.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arthur_Showdown_Game.Components.Game
+{
+    public partial class Tile
+    {
+        [Parameter] public int Indice { get; set; }
+        [Parameter] public bool IsEspecial { get; set; } 
+        [Parameter] public bool IsPerigo { get; set; }   
+        [Parameter] public bool IsTremendo { get; set; } 
+
+        private string ObterClassesVisuais()
+        {
+            string classes = "";
+            if (IsEspecial) classes += " tile-special ";
+            if (IsPerigo) classes += " tile-danger ";
+            if (IsTremendo) classes += " tile-shake ";
+            return classes;
+        }
+    }
+}
