1b678c6e9bc25c58a34bc25b6656d083a50c7a8c
GregoryCMSV <121581844+GregoryCMSV@users.noreply.github.com>
2026-09-13 21:46:48 -0300

    Criado modais para scoreboard, loja e mapa
    Criado modais para scoreboard, loja e mapa

---
+++ Arthur_Showdown/Arthur_Showdown_Game/Components/UI/Modais/MapModal.razor.cs
@@ -0,0 +1,13 @@
+﻿using Arthur_Showdown_Shared.Dtos.Game;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arthur_Showdown_Game.Components.UI.Modais
+{
+    public partial class MapModal
+    {
+        [Parameter] public PersonagemDto? PersonagemAtual { get; set; }
+    }
+}
---
+++ Arthur_Showdown/Arthur_Showdown_Game/Components/UI/Modais/ModalComponent.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arthur_Showdown_Game.Components.UI.Modais
+{
+    public partial class ModalComponent
+    {
+        [Parameter] public string Titulo { get; set; } = "";
+        [Parameter] public string CssClass { get; set; } = "generic-modal";
+        [Parameter] public bool MostrarDicaFechar { get; set; } = true;
+        [Parameter] public RenderFragment? ChildContent { get; set; }
+    }
+}
